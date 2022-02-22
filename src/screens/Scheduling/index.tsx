6cdc141669f2758688dcff2ca50d7e5fd3eb191c
marcelogaldino <marcelinhomgo@gmail.com>
2022-02-21 22:53:05 -0300

    feat: Page Car Scheduling, Header and footer implemented

---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { useTheme } from 'styled-components'
+
+import { BackButton } from '../../components/BackButton'
+
+import ArrowSvg from '../../assets/arrow.svg'
+
+import {
+    Container,
+    Header,
+    Title,
+    RentalPeriod,
+    DateInfo,
+    DateTitle,
+    DateValue,
+    Content,
+    Footer,
+} from './styles'
+import { StatusBar } from 'react-native'
+import { Button } from '../../components/Button'
+
+export function Scheduling() {
+    const theme = useTheme()
+    return (
+        <Container>
+            <StatusBar
+                barStyle='light-content'
+                backgroundColor='transparent'
+                translucent
+            />
+            <Header>
+                <BackButton
+                    onPress={() => { }}
+                    color={theme.colors.shape}
+                />
+
+                <Title>
+                    Escolha uma {'\n'}
+                    data de início e {'\n'}
+                    fim do aluguel
+                </Title>
+
+                <RentalPeriod>
+                    <DateInfo>
+                        <DateTitle>DE</DateTitle>
+                        <DateValue selected={true}>22/02/2022</DateValue>
+                    </DateInfo>
+
+                    <ArrowSvg />
+
+                    <DateInfo>
+                        <DateTitle>ATÉ</DateTitle>
+                        <DateValue selected={false}></DateValue>
+                    </DateInfo>
+                </RentalPeriod>
+            </Header>
+
+            <Content>
+
+            </Content>
+
+            <Footer>
+                <Button
+                    name="Confirmar"
+                />
+            </Footer>
+        </Container>
+    )
+}
