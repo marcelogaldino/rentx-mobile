005d38154037ac7478ceb5fe02172910f15a1334
marcelogaldino <marcelinhomgo@gmail.com>
2022-02-27 15:00:55 -0300

    feat: Confirm Button Component and SchedullingComplete screen

--- App.tsx
+++ App.tsx
@@ -17,6 +17,8 @@ import theme from './src/styles/theme';
 import { Home } from './src/screens/Home';
 import { CarDetails } from './src/screens/CarDetails';
 import { Scheduling } from './src/screens/Scheduling';
+import { SchedulingDetails } from './src/screens/SchedulingDetails';
+import { SchedulingComplete } from './src/screens/SchedulingComplete';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -35,7 +37,9 @@ export default function App() {
     <ThemeProvider theme={theme}>
       {/* <Home /> */}
       {/* <CarDetails /> */}
-      <Scheduling />
+      {/* <Scheduling /> */}
+      {/* <SchedulingDetails /> */}
+      <SchedulingComplete />
     </ThemeProvider>
   )
 }
---
+++ src/screens/SchedulingDetails/index.tsx
@@ -0,0 +1,130 @@
+import React from "react";
+import { StatusBar } from 'react-native';
+import { Feather } from '@expo/vector-icons'
+import { useTheme } from 'styled-components'
+
+import { Acessory } from "../../components/Acessory";
+import { BackButton } from "../../components/BackButton";
+import { ImageSlider } from "../../components/ImageSlider";
+
+import speedSvg from '../../assets/speed.svg'
+import accelerationSvg from '../../assets/acceleration.svg'
+import forceSvg from '../../assets/force.svg'
+import gasolineSvg from '../../assets/gasoline.svg'
+import exchangeSvg from '../../assets/exchange.svg'
+import peopleSvg from '../../assets/people.svg'
+
+import { Button } from "../../components/Button";
+
+import {
+    Container,
+    Header,
+    CarImages,
+    Content,
+    Details,
+    Brand,
+    Name,
+    Description,
+    Rent,
+    Period,
+    Price,
+    Acessories,
+    RentalPeriod,
+    CalendarIcon,
+    DateInfo,
+    DateTitle,
+    DateValue,
+    RentalPrice,
+    RentalPriceLabel,
+    RentalPriceDetails,
+    RentalPriceQuota,
+    RentalPriceTotal,
+    Footer
+} from './styles'
+
+export function SchedulingDetails() {
+    const theme = useTheme()
+
+    return (
+        <Container>
+            <StatusBar
+                barStyle='dark-content'
+                backgroundColor='transparent'
+            />
+            <Header>
+                <BackButton
+                    onPress={() => { }}
+                />
+
+            </Header>
+
+            <CarImages>
+
+                <ImageSlider
+                    imageUrl={['https://png.monster/wp-content/uploads/2020/11/2018-audi-rs5-4wd-coupe-angular-front-5039562b.png']}
+                />
+            </CarImages>
+
+            <Content>
+                <Details>
+                    <Description>
+                        <Brand>Lamborghini</Brand>
+                        <Name>Huracan</Name>
+                    </Description>
+
+                    <Rent>
+                        <Period>Ao dia</Period>
+                        <Price>R$ 580</Price>
+                    </Rent>
+                </Details>
+
+                <Acessories>
+                    <Acessory name="380Km/h" icon={speedSvg} />
+                    <Acessory name="3.2s" icon={accelerationSvg} />
+                    <Acessory name="800 HP" icon={forceSvg} />
+                    <Acessory name="Gasolina" icon={gasolineSvg} />
+                    <Acessory name="Auto" icon={exchangeSvg} />
+                    <Acessory name="2 pessoas" icon={peopleSvg} />
+                </Acessories>
+
+                <RentalPeriod>
+                    <CalendarIcon>
+                        <Feather
+                            name="calendar"
+                            size={24}
+                            color={theme.colors.shape}
+                        />
+                    </CalendarIcon>
+
+                    <DateInfo>
+                        <DateTitle>De</DateTitle>
+                        <DateValue>18/06/2022</DateValue>
+                    </DateInfo>
+
+                    <Feather
+                        name="chevron-right"
+                        size={10}
+                        color={theme.colors.text}
+                    />
+
+                    <DateInfo>
+                        <DateTitle>De</DateTitle>
+                        <DateValue>18/06/2022</DateValue>
+                    </DateInfo>
+                </RentalPeriod>
+
+                <RentalPrice>
+                    <RentalPriceLabel>TOTAL</RentalPriceLabel>
+                    <RentalPriceDetails>
+                        <RentalPriceQuota>R$580 x3 diarias</RentalPriceQuota>
+                        <RentalPriceTotal>R$ 2.900,00</RentalPriceTotal>
+                    </RentalPriceDetails>
+                </RentalPrice>
+            </Content>
+
+            <Footer>
+                <Button name="Confirmar" />
+            </Footer>
+        </Container>
+    )
+}
